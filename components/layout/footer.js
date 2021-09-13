04887d4464b6c44d2710b6a3059c59bdeea285c4
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2021-09-13 17:39:34 +0100

    updated footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <div className="flex items-center space-x-1 text-center justify-center text-xs md:text-sm w-full  md:w-[97%] lg:w-[90%] xl:w-[70%] md:mr-auto md:ml-auto bg-white pb-5 mb-2">
       <p>Created with love by </p>
-      <NextLink href="https://ukasoanya.com/">
-        <p className="text-[#800000]">Charles Ukasoanya</p>
+      <NextLink href="https://ukasoanya.com/" passHref target="_blank">
+        <p className="text-[#800000] cursor-pointer">Charles Ukasoanya</p>
       </NextLink>
       <p> (For the Ukandu family)</p>
     </div>
